fc58e4faf490d8f2195d9445a70f3367fba44351
sentieonycdev <jed@sentieo.com>
2018-07-07 12:19:16 +0000

    initial commit

---
+++ todo_list.R
@@ -0,0 +1,37 @@
+
+#Tidy NLP To Do List
+
+#combine traditional NLP (counting and tagging) with FORECASTING of 
+#same: document sentiment / NER A scalable R library for rapid natural language processing, that fits into both the tidyverse philosophy and Python production systems.
+
+#Leverages the following packages:
+#  reticulate (spacy, genism)
+#tidytext
+#textclean
+#sentimentr
+
+#Functionality:
+#  Sentiment analysis, with valences
+#PCA
+#broom implementations
+
+#Differences in functionality as compared to cleanNLP:
+#  Only one engine, spacy, rather than offer alternatives
+#words based on a corpus history
+
+##############################################
+#WORFLOW EXAMPLE
+
+#given a corpus of time series documents
+#track keyword changes over time
+#track entities over time
+#track sentiment over time
+#track sentiment by entity over time
+#predict entity ranking
+#predict keyword ranking
+#predict document sentiment trends
+#detect seasonal trends
+
+
+
+
